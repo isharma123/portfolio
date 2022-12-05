659ee87e8c4ad30478f16847771f6b34c3ad797e
isharma123 <119634627+isharma123@users.noreply.github.com>
2022-12-05 17:18:10 -0600

    Update presentations.vue

--- pages/presentations.vue
+++ pages/presentations.vue
@@ -3,11 +3,9 @@
     <div class="heading my-3">
             Video Presentation
     </div>
-    <div class="video">
-      <video width="550" height="350" controls>
-        <source src="https://youtu.be/o94qJGT3Vqg" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+    <div class="mx-auto">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/o94qJGT3Vqg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
     </div>
     <p class="description">
         As my major is Hospitality and Events Management, 
